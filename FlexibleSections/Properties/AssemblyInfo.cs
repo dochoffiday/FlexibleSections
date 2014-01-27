f73f1c963327330bc1935b529e1f644d1e760922
AJ Hoffer <mrhoffer@gmail.com>
2014-01-27 16:24:27 -0500

    nuget

--- FlexibleSections/Properties/AssemblyInfo.cs
+++ FlexibleSections/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ContentSection")]
+[assembly: AssemblyTitle("FlexibleSections")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ContentSection")]
+[assembly: AssemblyProduct("FlexibleSections")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
